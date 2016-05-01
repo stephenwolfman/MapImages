fad363d670acbea20bc7381d7c6efc908217f619
stephenwolfman <stephen.wolfman@blspm0564lax.blackline.corp>
2016-04-30 23:43:43 -0700

    Get port from env

--- server.js
+++ server.js
@@ -1,7 +1,4 @@
-//
-
-
-
+//Server.js
 var express = require("express");
 var logfmt = require("logfmt");
 var mapimages = require('./routes/mapimages');
@@ -12,5 +9,8 @@ app.get('/mapimages/sentby/:id', mapimages.findBySender);
 app.get('/mapimages/:id', mapimages.findById);
 app.get('/mapimages', mapimages.findAll);
 
-app.listen(1343);
-console.log('Listening on port 1343...');
+//Get port dynamically in prod/run on 1343 dev
+var port = Number(process.env.PORT || 1343);
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
